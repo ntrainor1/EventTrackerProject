c1168c248781918d247aff1f2b1d3f74d167e9b4
Nathaniel Trainor <natetrainor@msn.com>
2018-05-19 19:55:47 -0600

    Have DELETE, now UPDATE

--- ElectricBillingREST/src/main/webapp/js/electricScript.js
+++ ElectricBillingREST/src/main/webapp/js/electricScript.js
@@ -25,12 +25,14 @@ function index() {
 
 function displayBillList(billList) {
 	var list = document.getElementById('list');
-	var displayList = document.createElement('ul');
+	list.textContent = "";
+	
+	var displayTable = document.createElement('table');
 	billList.forEach(function(value) {
-		var displayListItem = document.createElement('li');
+		var displayListRow = document.createElement('tr');
+		var displayListItem = document.createElement('td');
 		displayListItem.setAttribute('class', 'yearItem');
 		displayListItem.textContent = value.year;
-		displayList.appendChild(displayListItem);
 		console.log(value);
 		displayListItem.addEventListener('click', function(event) {
 			event.preventDefault();
@@ -41,8 +43,11 @@ function displayBillList(billList) {
 				getBill(billId);
 			}
 		});
+		displayListRow.appendChild(displayListItem);
+		displayTable.appendChild(displayListRow);
 	});
-	list.appendChild(displayList);
+	
+	list.appendChild(displayTable);
 }
 
 function init() {
@@ -58,26 +63,34 @@ function init() {
 	document.initCreate.initialize.addEventListener('click', function(event) {
 		event.preventDefault();
 		var createDiv = document.getElementById('create');
+		createDiv.textContent = "";
+		
 		var createForm = document.createElement('form');
+		createForm.setAttribute('id', 'createForm');
 		var formYear = document.createElement('input');
 		formYear.setAttribute('name', 'year');
 		formYear.setAttribute('type', 'number');
+		formYear.required = true;
 		
 		var formCost = document.createElement('input');
 		formCost.setAttribute('name', 'cost');
 		formCost.setAttribute('type', 'number');
+		formCost.required = true;
 		
 		var formWattage = document.createElement('input');
 		formWattage.setAttribute('name', 'wattage');
 		formWattage.setAttribute('type', 'number');
+		formWattage.required = true;
 		
 		var formFirstName = document.createElement('input');
 		formFirstName.setAttribute('name', 'managerFirstName');
 		formFirstName.setAttribute('type', 'text');
+		formFirstName.required = true;
 		
 		var formLastName = document.createElement('input');
 		formLastName.setAttribute('name', 'managerLastName');
 		formLastName.setAttribute('type', 'text');
+		formLastName.required = true;
 		
 		var formSubmit = document.createElement('button');
 		formSubmit.setAttribute('name', 'submit');
@@ -91,10 +104,47 @@ function init() {
 		createForm.appendChild(formSubmit);
 		createDiv.appendChild(createForm);
 		
+		generateLabels();
 		establishCreateClickEvent();
-		index();
 	});
 };
+
+function generateLabels() {
+	var yearLabel = document.createElement("LABEL");
+    var yearText = document.createTextNode("Year ");
+    yearLabel.setAttribute("for", "year");
+    yearLabel.appendChild(yearText);
+    document.getElementById("createForm").insertBefore(yearLabel, document.getElementsByName("year")[0]);
+    document.getElementById("createForm").insertBefore(document.createElement("BR"), document.getElementsByName("year")[0]);
+    
+    var costLabel = document.createElement("LABEL");
+    var costText = document.createTextNode("Cost ");
+    costLabel.setAttribute("for", "cost");
+    costLabel.appendChild(costText);
+    document.getElementById("createForm").insertBefore(costLabel, document.getElementsByName("cost")[0]);
+    document.getElementById("createForm").insertBefore(document.createElement("BR"), document.getElementsByName("cost")[0]);
+    
+    var wattageLabel = document.createElement("LABEL");
+    var wattageText = document.createTextNode("Wattage ");
+    wattageLabel.setAttribute("for", "wattage");
+    wattageLabel.appendChild(wattageText);
+    document.getElementById("createForm").insertBefore(wattageLabel, document.getElementsByName("wattage")[0]);
+    document.getElementById("createForm").insertBefore(document.createElement("BR"), document.getElementsByName("wattage")[0]);
+    
+    var fNameLabel = document.createElement("LABEL");
+    var fNameText = document.createTextNode("Manager First Name ");
+    fNameLabel.setAttribute("for", "managerFirstName");
+    fNameLabel.appendChild(fNameText);
+    document.getElementById("createForm").insertBefore(fNameLabel, document.getElementsByName("managerFirstName")[0]);
+    document.getElementById("createForm").insertBefore(document.createElement("BR"), document.getElementsByName("managerFirstName")[0]);
+    
+    var lNameLabel = document.createElement("LABEL");
+    var lNameText = document.createTextNode("Manager Last Name ");
+    lNameLabel.setAttribute("for", "managerLastName");
+    lNameLabel.appendChild(lNameText);
+    document.getElementById("createForm").insertBefore(lNameLabel, document.getElementsByName("managerLastName")[0]);
+    document.getElementById("createForm").insertBefore(document.createElement("BR"), document.getElementsByName("managerLastName")[0]);
+}
 
 function establishCreateClickEvent() {
 	var createSubmitButton = document.getElementsByName("submit")[0];
@@ -112,7 +162,16 @@ function establishCreateClickEvent() {
 		var managerFirstName = createSubmitButton.parentElement.managerFirstName.value;
 		var managerLastName = createSubmitButton.parentElement.managerLastName.value;
 		console.log(year + cost + wattage + managerFirstName + managerLastName);
-		createBill(year, cost, wattage, managerFirstName, managerLastName);
+		
+		if (year != "" & cost != "" & wattage != "" & managerFirstName != "" & managerLastName != "") {
+			createBill(year, cost, wattage, managerFirstName, managerLastName);
+		}
+		else  {
+			var displayColumn = document.getElementById('display');
+			displayColumn.textContent = 'All fields must be filled in order to create a new bill entry.';
+		}
+		
+		index();
 	});
 }
 
@@ -138,6 +197,7 @@ function createBill(year, cost, wattage, managerFirstName, managerLastName) {
 			if (this.status === 200 || this.status === 201) {
 				var newBill = JSON.parse(this.responseText);
 				console.log(newBill);
+				index();
 				displayBill(newBill);
 			} else {
 				console.error("An error has occurred");
@@ -166,11 +226,13 @@ function getBill(billId) {
 			}
 		}
 	};
-	xhr.send(null);
+	xhr.send(billId);
 }
 
 function displayBill(bill) {
 	var displayColumn = document.getElementById('display');
+	
+	// DISPLAY INFORMATION
 	var billInfo = document.createElement('table');
 	billInfo.setAttribute('id', 'billTable');
 	displayColumn.textContent = '';
@@ -211,44 +273,54 @@ function displayBill(bill) {
 	billManagerRow.appendChild(billManagerNameColumn);
 	billManagerRow.appendChild(billManagerPropertyColumn);
 	billInfo.appendChild(billManagerRow);
+	
+	// ESTABLISH UPDATE BUTTON
+	var updateForm = document.createElement('form');
+	var updateSubmit = document.createElement('button');
+	updateSubmit.setAttribute('name', 'submit');
+	updateSubmit.textContent = "Update Bill";
+	//updateSubmit.addEventListener('click', function(event));
+	updateForm.appendChild(updateSubmit);
+	
+	// ESTABLISH DELETE BUTTON
+	var deleteForm = document.createElement('form');
+	var deleteSubmit = document.createElement('button');
+	deleteSubmit.setAttribute('name', 'submit');
+	deleteSubmit.textContent = "Delete Bill";
+	deleteSubmit.addEventListener('click', function(event) {
+		event.preventDefault();
+		
+		console.log(deleteSubmit.parentElement);
+		
+		if (confirm("Press OK if you are sure you wish to delete")) {
+			deleteBill(bill.id);
+		}
+		
+	});
+	deleteForm.appendChild(deleteSubmit);
 
 	displayColumn.appendChild(billInfo);
+//	displayColumn.appendChild(billInfo);
+	displayColumn.appendChild(deleteForm);
 }
 
-// function getActors(filmId) {
-//
-// var xhr = new XMLHttpRequest();
-// xhr.open('GET', 'api/films/' + filmId + '/actors', true);
-//
-// xhr.onreadystatechange = function() {
-// if (xhr.readyState === 4) {
-// if (xhr.status == 200 || xhr.status == 201) {
-// var cast = JSON.parse(xhr.responseText);
-// console.log(cast);
-// displayActors(cast);
-// } else {
-// console.log("Film not found.");
-// console.error(xhr.status + ': ' + xhr.responseText);
-// var filmData = document.getElementById('filmData');
-// filmData.textContent = 'Film not found.';
-// }
-// }
-// };
-// xhr.send(null);
-// }
-
-// function displayActors(cast) {
-// var dataDiv = document.getElementById('actorData');
-// var actorList = document.createElement('ul');
-// dataDiv.textContent = '';
-//
-// cast.forEach(function(value, index, array) {
-// var actor = document.createElement('li');
-// actor.textContent = value.firstName + " " + value.lastName;
-// console.log(actor);
-// actorList.appendChild(actor);
-// });
-//
-// dataDiv.appendChild(actorList);
-//
-// }
+function deleteBill(billId) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('DELETE', 'api/electricbills/' + billId, true);
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (this.status === 200) {
+				console.log(xhr.responseText);
+				if (xhr.responseText) {
+					index();
+				}
+			} else {
+				console.error("An error has occurred");
+				console.error(xhr.status + ': ' + xhr.responseText);
+			}
+		}
+	}
+	
+	xhr.send(billId);
+}
